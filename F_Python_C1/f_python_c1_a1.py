d285052b50b14b590c799273f624ade217cb8b6b
nsfoliveira <nsfoliveira90@gmail.com>
2021-05-16 12:57:27 -0300

    Create f_python_c1_a1.py
    primeiros passos

---
+++ F_Python_C1/f_python_c1_a1.py
@@ -0,0 +1,7 @@
+print("ola mundo")
+#para analisar alguma função,podemos chamar a função interativa help. Toda função tem que finalizar com ()
+print("natalia oliveirqa", "analista", sep=" - ")
+subst = "Python"
+verbo = "é"
+adjetivo = "fantástico"
+print(subst, verbo, adjetivo, sep="_", end="!\n")
