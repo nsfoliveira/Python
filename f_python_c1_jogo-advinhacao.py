ace18e3380d3403c1b06d507febc43aff4a2f4f7
Natália Oliveira <>
2021-05-19 16:54:29 +0000

    Estrutura basica, sem acerto

---
+++ f_python_c1_jogo-advinhacao.py
@@ -0,0 +1,10 @@
+print("---------------------------------")
+print("Bem vindo ao Jogo de Adivinhacao!")
+print("---------------------------------")
+
+num_sec = 28
+
+chute = input("Por favor, digite um numero: ")
+
+print("Voce digitou: ", chute)
+
