b79d87b808e7d50194c069921eb07e70158a7c44
nsfoliveira <nsfoliveira90@gmail.com>
2021-05-21 13:02:33 -0300

    correção de erro de variável

--- F_Python_C1/jogo_adivinhacao_vsc.py
+++ F_Python_C1/jogo_adivinhacao_vsc.py
@@ -9,13 +9,15 @@ rodada  = 1
 for rodada in range(1, total_tentativa + 1):
     print("\nTentativa {} de {}".format(rodada, total_tentativa)) #string interpolation
     chute = input("Digite um número de 1 a 100: ")
+    
     print("Você digito: ", chute)
     chute_convert = int(chute)
 
-    if(chute < 1 or > 100):
+    if(chute_convert < 1 or chute_convert > 100):
         print("Número Inválido. Você deve digitar um número de 1 a 100")
         continue #ele não quebra o laço
 
+    chute_convert = int(chute)
     acertou = chute_convert == num_secreto
     maiorq  = chute_convert > num_secreto
     menorq  = chute_convert < num_secreto
