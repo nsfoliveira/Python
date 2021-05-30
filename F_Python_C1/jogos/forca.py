8344c4e33f1a983d3796556dd3c4ca51dd9462e7
nsfoliveira <nsfoliveira90@gmail.com>
2021-05-30 18:30:13 -0300

    loop

--- F_Python_C1/jogos/forca.py
+++ F_Python_C1/jogos/forca.py
@@ -3,6 +3,13 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = "dilminha"
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        print("jogando...")
+
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
