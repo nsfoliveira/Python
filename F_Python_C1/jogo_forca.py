4893618cdf3871a29e1be315a01b39c8de3adfe1
nsfoliveira <nsfoliveira90@gmail.com>
2021-05-30 18:11:51 -0300

    deletando

--- F_Python_C1/jogo_forca.py
+++
@@ -1,8 +0,0 @@
-def jogar():
-    print("\n***************************")
-    print("Bem Vindo ao jogo de Forca!")
-    print("***************************")
-    print("\nFim do Jogo!!\n")
-
-if(__name__=="__main__")
-    jogar()
