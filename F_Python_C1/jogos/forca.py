2bc594ed2cbb7a18ffc5ba28b8323d529c13b047
nsfoliveira <nsfoliveira90@gmail.com>
2021-06-12 11:23:05 -0300

    manipulando strings

--- F_Python_C1/jogos/forca.py
+++ F_Python_C1/jogos/forca.py
@@ -1,4 +1,4 @@
-def jogar():
+def jogar ():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
@@ -7,17 +7,21 @@ def jogar():
     enforcou = False
     acertou = False
 
-    while(not enforcou and not acertou):
+    while (not enforcou and not acertou):
 
         chute = input("Qual a letra? ")
+        chute = chute.strip()  # para tratar a variável caso tenha espaços
+
+        index = 0  # posicionador
 
         for letra in palavra_secreta:
-            if(chute == letra):
-                print("Encontrei a letra {} na posição {}")
+            if (chute.upper() == letra.upper()): #convertendo as variaveis em maiusculas
+                print("Encontrei a letra {} na posição {}".format(letra, index))
             index = index + 1
         print("jogando...")
 
     print("Fim do jogo")
 
-if(__name__ == "__main__"):
+
+if (__name__ == "__main__"):
     jogar()
