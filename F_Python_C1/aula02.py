deff746758d32f7590d771938a43afd4f598e2c1
nsfoliveira <nsfoliveira90@gmail.com>
2021-07-03 15:17:30 -0300

    Update aula02.py

--- F_Python_C1/aula02.py
+++ F_Python_C1/aula02.py
@@ -1,5 +1,5 @@
-a = 10
-b = 5
+a = int(input('Entre com os valores: '))
+b = int(input('Entre com os valores: '))
 soma = a + b
 subtração = a - b
 multiplicação = a * b
@@ -12,4 +12,4 @@ print(multiplicação)
 print(divisão)
 print(resto)
 print(type(soma)) #type função para retornar o tipo da variável
-print('Soma: ', soma)
+print('soma: {}. subtração: {}'.format(soma, subtração)) #ele consegue concatenar independente do formato.
