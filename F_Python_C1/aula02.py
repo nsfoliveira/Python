ea6368f8902c044fc792ece199502411d8892f59
nsfoliveira <nsfoliveira90@gmail.com>
2021-07-03 14:56:19 -0300

    Create aula02.py

---
+++ F_Python_C1/aula02.py
@@ -0,0 +1,15 @@
+a = 10
+b = 5
+soma = a + b
+subtração = a - b
+multiplicação = a * b
+divisão = a / b
+resto = a % b
+
+print(soma)
+print(subtração)
+print(multiplicação)
+print(divisão)
+print(resto)
+print(type(soma)) #type função para retornar o tipo da variável
+print('Soma: ', soma)
