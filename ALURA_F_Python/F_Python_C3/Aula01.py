3dac7d28993b88d1797d3f0c1a659043e2e03ac7
nsfoliveira <nsfoliveira90@gmail.com>
2021-07-03 16:45:48 -0300

    update

--- ALURA_F_Python/F_Python_C3/Aula01.py
+++ ALURA_F_Python/F_Python_C3/Aula01.py
@@ -1,3 +1,3 @@
-from cria_conta import cria_conta
+from funcao_conta import cria_conta
 conta1 = cria_conta(6587, "Marco", 55.00, 1800)
 conta1["numero"]
--- ALURA_F_Python/F_Python_C3/funcao_conta.py
+++ ALURA_F_Python/F_Python_C3/funcao_conta.py
@@ -1,3 +1,5 @@
 def cria_conta(numero, titular, saldo, limite):
     conta =  {"numero": 6265, "titular": "Natália", "saldo": 150.00, "limite": 1000.00}
     return (conta)
+
+def
