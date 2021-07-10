0d2c6fa9590bdf3e9a65d2146b9ef018bb55c948
nsfoliveira <nsfoliveira90@gmail.com>
2021-07-10 15:47:27 -0300

    update

--- ALURA_F_Python/F_Python_C3/Aula01.py
+++
@@ -1,6 +0,0 @@
-from funcao_conta import cria_conta, deposita, saca, extrato
-conta = cria_conta(123, "Natalia", 150.0, 1000.0)
-deposita(conta, 300.0)
-extrato(conta)
-saca(conta, 100.0)
-extrato(conta)
--- ALURA_F_Python/F_Python_C3/Executando.py
+++ ALURA_F_Python/F_Python_C3/Executando.py
@@ -3,3 +3,15 @@ conta = Conta(123, "Nico", 55.5, 1000.0)
 conta2 = Conta(321, "Marcos", 100.0, 1000.0)
 conta.titular
 conta2.titular
+
+conta.extrato()
+
+conta2.extrato()
+
+conta.deposita(15.0)
+
+conta.extrato()
+
+conta.saca(10.0)
+conta.extrato()
+
