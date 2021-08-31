3f1cf87cca78c18d66aa78f23e3e6f4e3c2d377e
nsfoliveira <nsfoliveira90@gmail.com>
2021-08-31 20:36:43 -0300

    .

--- DSA_AnaliseDeDadosComPython/cap4_01.py
+++ DSA_AnaliseDeDadosComPython/cap4_01.py
@@ -2,3 +2,13 @@
 # Versão da Linguagem Python
 from platform import python_version
 print('Versão da Linguagem Python Usada Neste Jupyter Notebook:', python_version())
+
+import datetime
+
+agora = datetime.datetime.now()
+
+print (agora)
+
+tempo = datetime.time(7,8,12)
+
+print (tempo)
